d379cc6addefd6b9ae2414d880f6c103945e2997
fernandez <fernandez@ai.wu.ac.at>
2018-04-27 10:12:34 -0700

    add counts of links

--- libhdt/tools/hdtSearchLinksPerDomain.cpp
+++ libhdt/tools/hdtSearchLinksPerDomain.cpp
@@ -97,7 +97,8 @@ int main(int argc, char **argv) {
 				hdt_file->getDictionary()->getNshared());
 
 		if (verbose) {
-			cout << endl << "(verbose)- Let's check the content of our ranges:" << endl;
+			cout << endl << "(verbose)- Let's check the content of our ranges:"
+					<< endl;
 			domains.verbose_print();
 			cout << endl << endl;
 
@@ -111,7 +112,8 @@ int main(int argc, char **argv) {
 
 		if (test) {
 
-			cout<< "(test)- See all links with ID="<<testId<<" and rol "<<rolString<<endl;
+			cout << "(test)- See all links with ID=" << testId << " and rol "
+					<< rolString << endl;
 			TripleComponentRole origin_rol = SUBJECT;
 			TripleComponentRole target_rol = OBJECT;
 
@@ -125,7 +127,7 @@ int main(int argc, char **argv) {
 					((BitmapTriples*) (hdt_file->getTriples()))->searchRange(
 							testId, testId, origin_rol);
 
-			unsigned int count=1;
+			unsigned int count = 1;
 			while (it->hasNext()) {
 				TripleID triple = *it->next();
 				unsigned int checkID = triple.getObject();
@@ -134,37 +136,82 @@ int main(int argc, char **argv) {
 				}
 				//cout<<"Let's check ID "<<checkID<<" as of "<<target_rol<<endl;
 				string target_domain = domains.getDomain(checkID, target_rol);
-				if (origin_rol==SUBJECT)
-					cout << "LINK " << count<<":"<<origin_domain << " --> " << target_domain<< endl;
-					else
-						cout << "LINK " << count<<":"<<origin_domain << " <-- " << target_domain<< endl;
+				if (origin_rol == SUBJECT)
+					cout << "LINK " << count << ":" << origin_domain << " --> "
+							<< target_domain << endl;
+				else
+					cout << "LINK " << count << ":" << origin_domain << " <-- "
+							<< target_domain << endl;
 				count++;
 			}
 
 			delete it;
 
 		} else {
-			unsigned int count=1;
+			unsigned int count = 1;
+			map<string, int> simple_links; //count the different simple links (domain-subject domain-object);
+			map<string, int> labeled_links; //count the different labeled links (domain-subject predicate domain-object);
+			map<string, int> domainLabeled_links; //count the different domain-labeled links (domain-subject domain-predicate domain-object);
+			//todo maybe compute also the type of the subject/object?
 
-			IteratorTripleID* it =
-					hdt_file->getTriples()->searchAll();
+			IteratorTripleID* it = hdt_file->getTriples()->searchAll();
 
-			string origin_domain="";
-			unsigned int prevSubject=0;
+			string origin_domain = "";
+			unsigned int prevSubject = 0;
 			while (it->hasNext()) {
 				TripleID triple = *it->next();
-				if ((triple.getSubject()!=prevSubject) && (triple.getSubject()>=domains.getNext_range())) {
+				if ((triple.getSubject() != prevSubject)
+						&& (triple.getSubject() >= domains.getNext_range())) {
 					// some optimization to avoid more checks
 					// - we look that the triple is different than the previous one
 					// - we look that we have to enter into the next range
 					// otherwise we don't get a new domain and we keep the previous
-					origin_domain = domains.getDomain(triple.getSubject(), SUBJECT);
+					origin_domain = domains.getDomain(triple.getSubject(),
+							SUBJECT);
 					prevSubject = triple.getSubject();
 				}
-				string target_domain = domains.getDomain(triple.getObject(), OBJECT);
-				cout << "LINK " << count<<":"<<origin_domain << " --> " << target_domain<< endl;
+				string target_domain = domains.getDomain(triple.getObject(),
+						OBJECT);
+				cout << "LINK " << count << ":" << origin_domain << " --> "
+						<< target_domain << endl;
 				count++;
+
+				//store in the maps
+				string simple_link = origin_domain + " " + target_domain; // we assume the space is not found in the domains
+				string labeled_link = origin_domain + " "
+						+ hdt_file->getDictionary()->idToString(
+								triple.getPredicate(), PREDICATE) + " "
+						+ target_domain; // we assume the space is not found in the domains
+				string domainLabeled_link = origin_domain + " "
+						+ domains.getDomain(triple.getPredicate(), PREDICATE)
+						+ " " + target_domain; // we assume the space is not found in the domains
+
+				simple_links[simple_link] = simple_links[simple_link] + 1;
+				labeled_links[labeled_link] = labeled_links[labeled_link] + 1;
+				domainLabeled_links[domainLabeled_link] =
+						domainLabeled_links[domainLabeled_link] + 1;
 			}
+
+			// Print stats
+			cout<<"-------------------------------------"<<endl;
+			cout << endl << endl << "SIMPLE LINKS" << endl;
+			for (std::map<string, int>::iterator it = simple_links.begin();
+					it != simple_links.end(); ++it) {
+				std::cout << it->first << " => " << it->second << '\n';
+			}
+
+			cout << endl << endl << "LINKS WITH PREDICATES" << endl;
+			for (std::map<string, int>::iterator it = labeled_links.begin();
+					it != labeled_links.end(); ++it) {
+				std::cout << it->first << " => " << it->second << '\n';
+			}
+
+			cout << endl << endl << "LINKS WITH DOMAIN PREDICATES" << endl;
+			for (std::map<string, int>::iterator it = domainLabeled_links.begin();
+					it != domainLabeled_links.end(); ++it) {
+				std::cout << it->first << " => " << it->second << '\n';
+			}
+			cout<<endl<<endl<<"-------------------------------------"<<endl<<endl;
 
 			delete it;
 		}
